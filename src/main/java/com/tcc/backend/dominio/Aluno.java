c12c11aca46e4e8d2a8c7e7e55bc6ca03ca62fbc
Gustavo Souza <gustavodonizetesouza@gmail.com>
2019-04-08 13:30:53 -0300

    teste de classe de dominio alunos

---
+++ src/main/java/com/tcc/backend/dominio/Aluno.java
@@ -0,0 +1,127 @@
+package com.tcc.backend.dominio;
+
+import java.io.Serializable;
+
+public class Aluno implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	Integer cpf;
+	String nome;
+	String endereco;
+	String cidade;
+	String uf;
+	String telefone;
+	String email;
+	String senha;
+	
+	public Aluno() {
+	}
+
+	public Aluno(Integer cpf, String nome, String endereco, String cidade, String uf, String telefone, String email,
+			String senha) {
+		super();
+		this.cpf = cpf;
+		this.nome = nome;
+		this.endereco = endereco;
+		this.cidade = cidade;
+		this.uf = uf;
+		this.telefone = telefone;
+		this.email = email;
+		this.senha = senha;
+	}
+
+	public Integer getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(Integer cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getUf() {
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (cpf == null) {
+			if (other.cpf != null)
+				return false;
+		} else if (!cpf.equals(other.cpf))
+			return false;
+		return true;
+	}
+
+	
+	
+}
--- src/main/java/com/tcc/backend/recursos/AlunoRecurso.java
+++ src/main/java/com/tcc/backend/recursos/AlunoRecurso.java
@@ -1,15 +1,35 @@
 package com.tcc.backend.recursos;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.tcc.backend.dominio.Aluno;
 
 @RestController
 @RequestMapping(value="/alunos")
 public class AlunoRecurso {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "REST Ok!";
+	public List<Aluno> listar() {
+		
+		Aluno aluno1 = new Aluno();
+		Aluno aluno2 = new Aluno();
+		
+		aluno1.setCpf(1);
+		aluno1.setNome("João");
+		
+		aluno2.setCpf(2);
+		aluno2.setNome("Maria");
+		
+		List<Aluno> lista = new ArrayList<>();
+		
+		lista.add(aluno1);
+		lista.add(aluno2);
+		
+		return lista;
 	}
 }
