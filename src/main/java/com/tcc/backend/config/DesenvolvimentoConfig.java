f42f8249e8efa67cd7b48cd6c44a906d864fc4a8
Gustavo Souza <gustavodonizetesouza@gmail.com>
2019-04-09 16:15:08 -0300

    configuracao inicial de login

--- src/main/java/com/tcc/backend/config/DesenvolvimentoConfig.java
+++ src/main/java/com/tcc/backend/config/DesenvolvimentoConfig.java
@@ -1,6 +1,7 @@
 package com.tcc.backend.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -8,14 +9,21 @@ import org.springframework.context.annotation.Profile;
 import com.tcc.backend.servicos.BancoDeDadosServico;
 
 @Configuration
-@Profile("teste")
+@Profile("desenvolvimento")
 public class DesenvolvimentoConfig {
 	
 	@Autowired
 	private BancoDeDadosServico bds;
 	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String estrategia;
+	
 	@Bean
 	public boolean instanciarBancoDeDados() {
+		
+		if (!"create".equals(estrategia)) {
+			return false;
+		}
 		bds.instanciarBancoDeDadosTeste();
 		return true;
 	}
